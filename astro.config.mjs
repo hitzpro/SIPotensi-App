4784debf5288a5bbaf32e0e4f6398883e4fbae1e
hitzpro <wakhidaziz95192@gmail.com>
2025-12-12 11:28:50 +0700

    Add Vercel Adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon'; // Pastikan ini ada
+import vercel from '@astrojs/vercel/serverless'; // 1. Import Adapter
 
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     tailwind(), 
     icon() // Pastikan ini ada di dalam array
