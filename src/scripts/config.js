e208ede2f5c3395a36078e45386bcc2977cc4130
hitzpro <wakhidaziz95192@gmail.com>
2025-12-12 11:24:36 +0700

    Fix build script and dynamic API URL

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,9 +1,8 @@
 // src/scripts/config.js
 export const API_CONFIG = {
-    BASE_URL: "http://127.0.0.1:3000/api", // Sesuai permintaan
-    TIMEOUT: 5000 // Opsional: batas waktu request
-  };
-
+  BASE_URL: import.meta.env.PUBLIC_API_URL || "http://127.0.0.1:3000/api",
+  TIMEOUT: 5000
+};
 export const KEY = {
   PUBLIC_VAPID_KEY : "BC2Cb1v_ycDo_mYoseEKMuWcmJDDkhKegGl_lVNbEISRnUGSoA6JJZWVBQhmGwbc-aP7NUKl0VF8ZNfxOZ0JRo0"
 }
